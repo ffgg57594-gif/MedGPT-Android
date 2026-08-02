465045a48bc300cd2548f96ab61d76b8b9384c1a
Arjun Rohan <ffgg57594@gmail.com>
2026-08-03 00:39:39 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.drarabi.medvision"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.2.3"
 
         // NDK/CMake for native security layer
         externalNativeBuild {
